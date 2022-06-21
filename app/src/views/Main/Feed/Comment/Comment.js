95c799babb4afb216e69249aeb4c2a7e570b809c
vicky1104 <vicky114chauhan@gmail.com>
2022-06-21 14:33:10 +0530

    dd

--- app/src/views/Main/Feed/Comment/Comment.js
+++ app/src/views/Main/Feed/Comment/Comment.js
@@ -4,203 +4,204 @@ import React, { useEffect } from "react";
 import { View, StyleSheet, Dimensions, VirtualizedList } from "react-native";
 import { Entypo } from "@expo/vector-icons";
 import {
-	listComment,
-	postComment,
+  listComment,
+  postComment,
 } from "../../../../helper/services/CommentService";
 import { deleteComment } from "../../../../helper/services/DeleteComment";
 
 import { Text } from "native-base";
 import AppInput from "../../../../constants/components/ui-component/AppInput";
 import ReactionSheet from "./ReactionSheet";
+import AllComments from "./AllComments";
 
 function Comment(props) {
-	const { params } = useRoute();
-	const [text, setText] = React.useState("");
-	const [height, setHeight] = React.useState(0);
-	const [data, setData] = React.useState([]);
-	const [postId] = React.useState(params?.postId);
-	const refRBSheet = React.useRef(null);
-	const [open, setOpen] = React.useState(false);
-	const [commentId, setCommentId] = React.useState();
-	const [reply, setReply] = React.useState(false);
-	const [delComment, setDelComment] = React.useState(false);
+  const { params } = useRoute();
+  const [text, setText] = React.useState("");
+  const [height, setHeight] = React.useState(0);
+  const [data, setData] = React.useState([]);
+  const [postId] = React.useState(params?.postId);
+  const refRBSheet = React.useRef(null);
+  const [open, setOpen] = React.useState(false);
+  const [commentId, setCommentId] = React.useState();
+  const [reply, setReply] = React.useState(false);
+  const [delComment, setDelComment] = React.useState(false);
 
-	useEffect(() => getComments(), [getComments]);
+  useEffect(() => getComments(), [getComments]);
 
-	const submitComment = React.useCallback(() => {
-		if (text.length > 0) {
-			postComment({
-				feedId: postId,
-				text: text,
-			})
-				.then((res) => {
-					setText("");
-					getComments();
-				})
-				.catch((err) => console.log(err));
-		} else {
-			ToastMessage("type comment");
-		}
-	}, [text, postId]);
+  const submitComment = React.useCallback(() => {
+    if (text.length > 0) {
+      postComment({
+        feedId: postId,
+        text: text,
+      })
+        .then((res) => {
+          setText("");
+          getComments();
+        })
+        .catch((err) => console.log(err));
+    } else {
+      ToastMessage("type comment");
+    }
+  }, [text, postId]);
 
-	const getComments = React.useCallback(() => {
-		listComment(postId)
-			.then((res) => {
-				// console.log(res);
-				setData(res);
-			})
-			.catch((err) => console.log(err));
-	}, [postId]);
+  const getComments = React.useCallback(() => {
+    listComment(postId)
+      .then((res) => {
+        // console.log(res);
+        setData(res);
+      })
+      .catch((err) => console.log(err));
+  }, [postId]);
 
-	function replyToComment() {
-		if (text.length > 0) {
-			postComment({
-				feedId: reply.feedId,
-				replyToCommentId: reply.replyToCommentId,
-				text: text,
-			})
-				.then((res) => {
-					setReply(false);
-					setText("");
-					getComments();
-				})
-				.catch((err) => console.warn(err));
-		}
-	}
+  function replyToComment() {
+    if (text.length > 0) {
+      postComment({
+        feedId: reply.feedId,
+        replyToCommentId: reply.replyToCommentId,
+        text: text,
+      })
+        .then((res) => {
+          setReply(false);
+          setText("");
+          getComments();
+        })
+        .catch((err) => console.warn(err));
+    }
+  }
 
-	function deleteCommentFx() {
-		deleteComment(commentId).then((res) => {
-			setDelComment(false);
-			getComments();
-		});
-	}
-	return (
-		<React.Fragment>
-			<LinearGradient
-				style={styles.container}
-				start={{ x: 0, y: 0 }}
-				end={{ x: 1, y: 1 }}
-				colors={["#ffffff", "#eeeeff"]}
-			>
-				<View style={{ paddingBottom: 70 }}>
-					{data?.length > 0 ? (
-						<VirtualizedList
-							initialScrollIndex={data.length - 1}
-							showsVerticalScrollIndicator={false}
-							getItemCount={(data) => data.length}
-							getItem={(data, index) => data[index]}
-							data={data}
-							ItemSeparatorComponent={() => (
-								<View
-									style={{
-										width: "100%",
-										height: 1,
-										backgroundColor: "#FFFFFF",
-									}}
-								/>
-							)}
-							keyExtractor={(data) => data.id}
-							renderItem={({ item }) => {
-								return (
-									<AllComments
-										refRBSheet={refRBSheet}
-										item={item}
-										commentOfComment={setText}
-										setOpen={setOpen}
-										setCommentId={setCommentId}
-										setReply={setReply}
-										setDelComment={setDelComment}
-									/>
-								);
-							}}
-						/>
-					) : (
-						<View
-							style={{
-								flex: 1,
-								justifyContent: "center",
-								alignItems: "center",
-							}}
-						>
-							<Text>Be the first one to comment...! </Text>
-						</View>
-					)}
-				</View>
+  function deleteCommentFx() {
+    deleteComment(commentId).then((res) => {
+      setDelComment(false);
+      getComments();
+    });
+  }
+  return (
+    <React.Fragment>
+      <LinearGradient
+        style={styles.container}
+        start={{ x: 0, y: 0 }}
+        end={{ x: 1, y: 1 }}
+        colors={["#ffffff", "#eeeeff"]}
+      >
+        <View style={{ paddingBottom: 70 }}>
+          {data?.length > 0 ? (
+            <VirtualizedList
+              initialScrollIndex={data.length - 1}
+              showsVerticalScrollIndicator={false}
+              getItemCount={(data) => data.length}
+              getItem={(data, index) => data[index]}
+              data={data}
+              ItemSeparatorComponent={() => (
+                <View
+                  style={{
+                    width: "100%",
+                    height: 1,
+                    backgroundColor: "#FFFFFF",
+                  }}
+                />
+              )}
+              keyExtractor={(data) => data.id}
+              renderItem={({ item }) => {
+                return (
+                  <AllComments
+                    refRBSheet={refRBSheet}
+                    item={item}
+                    commentOfComment={setText}
+                    setOpen={setOpen}
+                    setCommentId={setCommentId}
+                    setReply={setReply}
+                    setDelComment={setDelComment}
+                  />
+                );
+              }}
+            />
+          ) : (
+            <View
+              style={{
+                flex: 1,
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              <Text>Be the first one to comment...! </Text>
+            </View>
+          )}
+        </View>
 
-				{/*  ```````````````` ADD COMMENT */}
+        {/*  ```````````````` ADD COMMENT */}
 
-				<View
-					style={{
-						width: (Dimensions.get("window").width = "95%"),
-						alignSelf: "center",
-						position: "absolute",
-						bottom: 20,
-					}}
-				>
-					{reply.feedId && (
-						<View
-							style={{
-								backgroundColor: "#84959f",
-								position: "relative",
-								marginBottom: -20,
-								height: 40,
-								borderTopLeftRadius: 8,
-								borderTopRightRadius: 8,
-							}}
-						>
-							<Entypo
-								style={{ position: "absolute", top: -8, right: -8, zIndex: 99 }}
-								name={"circle-with-cross"}
-								size={20}
-								onPress={() => setReply(false)}
-								color={color.black}
-							/>
-							<Text style={{ paddingLeft: 8 }} numberOfLines={2}>
-								{reply.text}
-							</Text>
-						</View>
-					)}
+        <View
+          style={{
+            width: (Dimensions.get("window").width = "95%"),
+            alignSelf: "center",
+            position: "absolute",
+            bottom: 20,
+          }}
+        >
+          {reply.feedId && (
+            <View
+              style={{
+                backgroundColor: "#84959f",
+                position: "relative",
+                marginBottom: -20,
+                height: 40,
+                borderTopLeftRadius: 8,
+                borderTopRightRadius: 8,
+              }}
+            >
+              <Entypo
+                style={{ position: "absolute", top: -8, right: -8, zIndex: 99 }}
+                name={"circle-with-cross"}
+                size={20}
+                onPress={() => setReply(false)}
+                color={color.black}
+              />
+              <Text style={{ paddingLeft: 8 }} numberOfLines={2}>
+                {reply.text}
+              </Text>
+            </View>
+          )}
 
-					<AppInput
-						value={text}
-						multiline={true}
-						iconColor={"grey"}
-						onPressIcon={() =>
-							reply.feedId ? replyToComment() : submitComment()
-						}
-						iconName={"send"}
-						style={{
-							// borderRadius: 20,
-							height: height,
-							maxHeight: 100,
-							minHeight: 40,
-						}}
-						placeholder={reply.feedId ? "Reply a comment" : "Tap to Comment "}
-						onChangeText={(e) => setText(e)}
-						onContentSizeChange={(e) =>
-							setHeight(e.nativeEvent.contentSize.height)
-						}
-					/>
-				</View>
-			</LinearGradient>
+          <AppInput
+            value={text}
+            multiline={true}
+            iconColor={"grey"}
+            onPressIcon={() =>
+              reply.feedId ? replyToComment() : submitComment()
+            }
+            iconName={"send"}
+            style={{
+              // borderRadius: 20,
+              height: height,
+              maxHeight: 100,
+              minHeight: 40,
+            }}
+            placeholder={reply.feedId ? "Reply a comment" : "Tap to Comment "}
+            onChangeText={(e) => setText(e)}
+            onContentSizeChange={(e) =>
+              setHeight(e.nativeEvent.contentSize.height)
+            }
+          />
+        </View>
+      </LinearGradient>
 
-			<ReactionSheet refRBSheet={refRBSheet} id={commentId} />
+      <ReactionSheet refRBSheet={refRBSheet} id={commentId} />
 
-			{delComment && (
-				<ConfirmModal
-					// title={`Are you sure, you want to unfriend ${user?.firstName} ?`}
-					setModalVisible={setDelComment}
-					btnLabel={"Delete"}
-					message={"Delete Comment"}
-					onPress={deleteCommentFx}
-				/>
-			)}
-		</React.Fragment>
-	);
+      {delComment && (
+        <ConfirmModal
+          // title={`Are you sure, you want to unfriend ${user?.firstName} ?`}
+          setModalVisible={setDelComment}
+          btnLabel={"Delete"}
+          message={"Delete Comment"}
+          onPress={deleteCommentFx}
+        />
+      )}
+    </React.Fragment>
+  );
 }
 
 const styles = StyleSheet.create({
-	container: { flex: 1, position: "relative" },
+  container: { flex: 1, position: "relative" },
 });
 
 export default Comment;
--- app/src/views/Main/Feed/ContentViewer.js
+++ app/src/views/Main/Feed/ContentViewer.js
@@ -11,132 +11,132 @@ import { cloudinaryFeedUrl } from "../../../utils/util-func/constantExport";
 const ViewportAwareImage = Viewport.Aware(View);
 
 function ContentViewer({ mimeType, src }) {
-	const { icons, playAudio, stopPlayer } = usePlayPauseAudio(src, true);
-	const videoRef = React.useRef(null);
-	const { addListener } = useNavigation();
+  const { icons, playAudio, stopPlayer } = usePlayPauseAudio(src, false);
+  const videoRef = React.useRef(null);
+  const { addListener } = useNavigation();
 
-	React.useEffect(() => {
-		const unsubscribe = addListener("blur", (e) => {
-			// e.preventDefault();
-			videoRef?.current?.stopAsync();
-			stopPlayer();
-		});
-		return unsubscribe;
-	}, [stopPlayer]);
+  React.useEffect(() => {
+    const unsubscribe = addListener("blur", (e) => {
+      // e.preventDefault();
+      videoRef?.current?.stopAsync();
+      stopPlayer();
+    });
+    return unsubscribe;
+  }, [stopPlayer]);
 
-	const renderComp = () => {
-		if (mimeType && src) {
-			switch (mimeType) {
-				case "video/mp4":
-					return (
-						<Video
-							ref={videoRef}
-							source={{ uri: cloudinaryFeedUrl(src, "video") }}
-							resizeMode="contain"
-							useNativeControls={true}
-							style={{
-								width: "100%",
-								aspectRatio: 1,
-							}}
-							onError={(err) => {
-								console.log(err);
-							}}
-						/>
-					);
-				case "audio/mp3":
-					return (
-						<View
-							style={{
-								height: 364,
-								width: "100%",
-								display: "flex",
-								flexDirection: "row",
-								alignItems: "center",
-								flex: 1,
-							}}
-						>
-							<View
-								style={{
-									flexDirection: "row",
-									alignItems: "center",
-									position: "relative",
-									height: 60,
-								}}
-							>
-								<Image
-									style={{ width: "100%", borderRadius: 4, height: "100%" }}
-									source={require("../../../../assets/EditDrawerIcon/feedAudioBg.png")}
-								/>
-								<View
-									style={{
-										position: "absolute",
-										display: "flex",
-										flexDirection: "row",
-										height: 60,
-										width: "100%",
-										padding: 8,
-									}}
-								>
-									<TouchableOpacity
-										onPress={() => playAudio(src, "feed_audio")}
-									>
-										<View
-											style={{
-												marginRight: 10,
-												width: 44,
-												height: 44,
-												display: "flex",
-												flexDirection: "row",
-												justifyContent: "center",
-												alignItems: "center",
-												backgroundColor: "#FFF",
-												borderRadius: 50,
-											}}
-										>
-											<MaterialCommunityIcons name={icons} size={30} />
-										</View>
-									</TouchableOpacity>
-									{icons === "pause" && (
-										<Image
-											style={{
-												height: "80%",
-												width: "85%",
-												alignSelf: "center",
-											}}
-											source={require("../../../../assets/Animations/wave.gif")}
-										/>
-									)}
-								</View>
-							</View>
-						</View>
-					);
-				case "image/jpg":
-					return <FeedZoom imageUri={cloudinaryFeedUrl(src, "image")} />;
-			}
-		} else return null;
-	};
+  const renderComp = () => {
+    if (mimeType && src) {
+      switch (mimeType) {
+        case "video/mp4":
+          return (
+            <Video
+              ref={videoRef}
+              source={{ uri: cloudinaryFeedUrl(src, "video") }}
+              resizeMode="contain"
+              useNativeControls={true}
+              style={{
+                width: "100%",
+                aspectRatio: 1,
+              }}
+              onError={(err) => {
+                console.log(err);
+              }}
+            />
+          );
+        case "audio/mp3":
+          return (
+            <View
+              style={{
+                height: 364,
+                width: "100%",
+                display: "flex",
+                flexDirection: "row",
+                alignItems: "center",
+                flex: 1,
+              }}
+            >
+              <View
+                style={{
+                  flexDirection: "row",
+                  alignItems: "center",
+                  position: "relative",
+                  height: 60,
+                }}
+              >
+                <Image
+                  style={{ width: "100%", borderRadius: 4, height: "100%" }}
+                  source={require("../../../../assets/EditDrawerIcon/feedAudioBg.png")}
+                />
+                <View
+                  style={{
+                    position: "absolute",
+                    display: "flex",
+                    flexDirection: "row",
+                    height: 60,
+                    width: "100%",
+                    padding: 8,
+                  }}
+                >
+                  <TouchableOpacity
+                    onPress={() => playAudio(src, "feed_audio")}
+                  >
+                    <View
+                      style={{
+                        marginRight: 10,
+                        width: 44,
+                        height: 44,
+                        display: "flex",
+                        flexDirection: "row",
+                        justifyContent: "center",
+                        alignItems: "center",
+                        backgroundColor: "#FFF",
+                        borderRadius: 50,
+                      }}
+                    >
+                      <MaterialCommunityIcons name={icons} size={30} />
+                    </View>
+                  </TouchableOpacity>
+                  {icons === "pause" && (
+                    <Image
+                      style={{
+                        height: "80%",
+                        width: "85%",
+                        alignSelf: "center",
+                      }}
+                      source={require("../../../../assets/Animations/wave.gif")}
+                    />
+                  )}
+                </View>
+              </View>
+            </View>
+          );
+        case "image/jpg":
+          return <FeedZoom imageUri={cloudinaryFeedUrl(src, "image")} />;
+      }
+    } else return null;
+  };
 
-	return (
-		<ViewportAwareImage
-			onViewportEnter={() => {
-				if (mimeType === "video/mp4") {
-					videoRef?.current?.playAsync();
-				} else {
-					playAudio();
-				}
-			}}
-			onViewportLeave={() => {
-				if (mimeType === "video/mp4") {
-					videoRef?.current?.stopAsync();
-				} else {
-					stopPlayer();
-				}
-			}}
-			style={{ flex: 1 }}
-		>
-			{renderComp()}
-		</ViewportAwareImage>
-	);
+  return (
+    <ViewportAwareImage
+      onViewportEnter={() => {
+        if (mimeType === "video/mp4") {
+          videoRef?.current?.playAsync();
+        } else {
+          playAudio();
+        }
+      }}
+      onViewportLeave={() => {
+        if (mimeType === "video/mp4") {
+          videoRef?.current?.stopAsync();
+        } else {
+          stopPlayer();
+        }
+      }}
+      style={{ flex: 1 }}
+    >
+      {renderComp()}
+    </ViewportAwareImage>
+  );
 }
 
 export default ContentViewer;
