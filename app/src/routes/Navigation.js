23f8b296e586c2c9c4dda1f2514f4c32942f3baf
vicky1104 <vicky114chauhan@gmail.com>
2022-07-06 14:45:15 +0530

    changes agora group video call

--- app/src/routes/Navigation.js
+++ app/src/routes/Navigation.js
@@ -1,6 +1,7 @@
 import Splash from "../views/Splash/Splash";
 import { AuthNavJson } from "./Navigation/AuthNavJson";
 import BottomNavigation from "./Navigation/BottomNavigation";
+import CallNavigation from "./Navigation/CallNavigation";
 import Contacts from "./Navigation/ContactsNavigation";
 import FeedNavigation from "./Navigation/FeedNavigation";
 import { MapNavigation } from "./Navigation/MapNavigation";
@@ -8,20 +9,21 @@ import ProfileNavigation from "./Navigation/ProfileNavigation";
 import RoomNavigation from "./Navigation/RoomNavigation";
 
 export const NavigationJson = [
-	{
-		options: { headerShown: false },
-		name: "Splash",
-		component: Splash,
-	},
-	...AuthNavJson,
-	{
-		options: { headerShown: false },
-		name: "Bottom",
-		component: BottomNavigation,
-	},
-	...FeedNavigation,
-	...RoomNavigation,
-	...ProfileNavigation,
-	...Contacts,
-	...MapNavigation,
+  {
+    options: { headerShown: false },
+    name: "Splash",
+    component: Splash,
+  },
+  ...AuthNavJson,
+  {
+    options: { headerShown: false },
+    name: "Bottom",
+    component: BottomNavigation,
+  },
+  ...FeedNavigation,
+  ...RoomNavigation,
+  ...ProfileNavigation,
+  ...Contacts,
+  ...MapNavigation,
+  ...CallNavigation,
 ];
