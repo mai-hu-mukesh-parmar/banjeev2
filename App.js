663851cad973f5732026be7dc8d65f0f489c14b0
mai-hu-mukesh-parmar <mukesh.parmar@iwantunlimited.com>
2022-07-21 11:00:18 +0530

    changes notification

--- App.js
+++ App.js
@@ -9,27 +9,32 @@ import AuthSocket from "./app/src/Socket/Socket";
 import { SocketContext, socket } from "./app/src/Context/Socket";
 import SocketEvents from "./app/src/Socket/SocketEvents";
 import { LogBox } from "react-native";
+import Firebase from "./app/src/notification";
+import GetNotification from "./app/src/Socket/GetNotification";
 LogBox.ignoreLogs([
-  "ViewPropTypes will be removed from React Native. Migrate to ViewPropTypes exported from 'deprecated-react-native-prop-types'",
+	"ViewPropTypes will be removed from React Native. Migrate to ViewPropTypes exported from 'deprecated-react-native-prop-types'",
 ]);
 const App = () => {
-  console.log(Dimensions.get("screen").width);
+	// console.log(Dimensions.get("screen").width);
 
-  return (
-    <Provider store={store}>
-      <SocketContext.Provider value={socket}>
-        <AuthSocket>
-          <NavigationContainer>
-            <SocketEvents>
-              <NativeBaseProvider>
-                <NavigationView />
-              </NativeBaseProvider>
-            </SocketEvents>
-          </NavigationContainer>
-        </AuthSocket>
-      </SocketContext.Provider>
-    </Provider>
-  );
+	return (
+		<Provider store={store}>
+			<SocketContext.Provider value={socket}>
+				<AuthSocket>
+					<Firebase />
+					<GetNotification>
+						<NavigationContainer>
+							<SocketEvents>
+								<NativeBaseProvider>
+									<NavigationView />
+								</NativeBaseProvider>
+							</SocketEvents>
+						</NavigationContainer>
+					</GetNotification>
+				</AuthSocket>
+			</SocketContext.Provider>
+		</Provider>
+	);
 };
 
 export default App;
